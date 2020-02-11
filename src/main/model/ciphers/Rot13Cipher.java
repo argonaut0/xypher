a68cf4fb6dc1b22eb293ccb537e5ece4e75e201b
Jason Hsu <jasonhsu2013@gmail.com>
2020-02-11 12:51:08 -0800

    Add documentation to ROT13

--- src/main/model/ciphers/Rot13Cipher.java
+++ src/main/model/ciphers/Rot13Cipher.java
@@ -1,13 +1,17 @@
 package model.ciphers;
 
 /**
- * Represents an implementation of the ROT13 Cipher
+ * Represents an implementation of the ROT13 Cipher, a Caesar Cipher with shift 13
  * Reference: http://practicalcryptography.com/ciphers/classical-era/rot13/
  *
  * @author Jason Hsu
  */
 public class Rot13Cipher extends CaesarCipher {
 
+    /**
+     * MODIFIES: this
+     * EFFECTS: Constructs a Rot13 Cipher
+     */
     public Rot13Cipher() {
         super(13, "Rot13Cipher");
     }
