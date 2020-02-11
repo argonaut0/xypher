dd2692a80f47781f6441941458fb987afe39eec0
Jason Hsu <jasonhsu2013@gmail.com>
2020-02-11 12:46:36 -0800

    Add ROT13 tests

--- src/test/model/ciphers/Rot13Test.java
+++ src/test/model/ciphers/Rot13Test.java
@@ -1,5 +1,34 @@
 package model.ciphers;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Rot13Test {
-    // todo
+    private Rot13Cipher rot13Cipher;
+
+    @BeforeEach
+    void runBefore() {
+        rot13Cipher = new Rot13Cipher();
+    }
+
+    @Test
+    void nameTest() {
+        assertEquals("Rot13Cipher", rot13Cipher.toString());
+    }
+
+    @Test
+    void encodeTest() {
+        assertEquals("NOPQ", rot13Cipher.encode("ABCD"));
+        assertEquals("URYYB JBEYQ", rot13Cipher.encode("HELLO WORLD"));
+        assertEquals("NGGNPX NG QNJA", rot13Cipher.encode("ATTACK AT DAWN"));
+    }
+
+    @Test
+    void decodeTest() {
+        assertEquals("ABCD", rot13Cipher.decode("NOPQ"));
+        assertEquals("HELLO WORLD", rot13Cipher.decode("URYYB JBEYQ"));
+        assertEquals("ATTACK AT DAWN", rot13Cipher.decode("NGGNPX NG QNJA"));
+    }
 }
