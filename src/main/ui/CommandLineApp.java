ea1017099f0c0a5ca5d1e1c60810d645e2844462
Jason Hsu <jasonhsu2013@gmail.com>
2020-02-12 14:40:29 -0800

    Implement command line interface for sequences

--- src/main/ui/CommandLineApp.java
+++ src/main/ui/CommandLineApp.java
@@ -7,6 +7,7 @@ import model.ciphers.Cipher;
 import model.ciphers.Rot13Cipher;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -19,6 +20,7 @@ public class CommandLineApp {
             "0 : AtbashCipher\n"
                     + "1 : Rot13Cipher\n"
                     + "2 : CaesarCipher";
+    private static final String TRANSFORM_TEXT_PROMPT = "Enter your text to transform [A-Z] [a-z]:";
     private ArrayList<CipherSequence> sequences;
     private ArrayList<Cipher> ciphers;
     private Scanner scanner;
@@ -26,6 +28,7 @@ public class CommandLineApp {
 
 
     /**
+     * MODIFIES: this
      * EFFECTS: Sets up fields and runs app
      */
     public CommandLineApp() {
@@ -38,6 +41,7 @@ public class CommandLineApp {
     }
 
     /**
+     * REQUIRES: Legal console input
      * MODIFIES: this
      * EFFECTS: Runs a application loop
      */
@@ -56,7 +60,7 @@ public class CommandLineApp {
     private void printMenu() {
         System.out.println(MENU_DIVIDER);
         System.out.println("Choose an action:");
-        //System.out.println("m : Manage Sequences"); todo
+        System.out.println("m : Manage Sequences");
         System.out.println("c : Configure a new Cipher");
         System.out.println("e : Encode a string");
         System.out.println("d : Decode a string");
@@ -93,15 +97,32 @@ public class CommandLineApp {
     }
 
     /**
-     * todo
+     * REQUIRES: Legal console input
+     * EFFECTS: Processes input to manage the sequences
      */
     private void manage() {
-        System.out.println("Sequences: " + sequences);
-        System.out.println("WIP");
-        //todo
+        System.out.println("Choose an action:");
+        System.out.println("l : List sequences");
+        System.out.println("n : New sequence");
+        System.out.println("m : Modify sequence");
+        switch (scanner.nextLine()) {
+            case "l":
+                printList(sequences);
+                manage();
+                break;
+            case "n":
+                newSequence();
+                break;
+            case "m":
+                modifySequence();
+                break;
+            default:
+                System.out.println("Command not recognized.");
+        }
     }
 
     /**
+     * REQUIRES: Legal console input
      * MODIFIES: this
      * EFFECTS: Adds a cipher to the working ciphers list
      */
@@ -129,43 +150,148 @@ public class CommandLineApp {
     }
 
     /**
+     * REQUIRES: Legal console input
      * EFFECTS: Encodes some text with a chosen cipher
      */
     private void encode() {
-        System.out.println("Choose a cipher: ");
-        printCiphers(ciphers);
         Cipher cipher = chooseCipher();
-        System.out.println("Enter your text to encode [A-Z] [a-z]:");
+        System.out.println(TRANSFORM_TEXT_PROMPT);
         System.out.println(cipher.encode(scanner.nextLine()));
     }
 
     /**
+     * REQUIRES: Legal console input
      * EFFECTS: Decodes some text with a chosen cipher
      */
     private void decode() {
-        System.out.println("Choose a cipher: ");
-        printCiphers(ciphers);
         Cipher cipher = chooseCipher();
-        System.out.println("Enter your text to decode [A-Z] [a-z]:");
+        System.out.println(TRANSFORM_TEXT_PROMPT);
         System.out.println(cipher.decode(scanner.nextLine()));
     }
 
     /**
+     * REQUIRES: Legal console input
      * EFFECTS: Gets the cipher chosen by user
      * @return The cipher from the working list chosen by user
      */
     private Cipher chooseCipher() {
+        System.out.println("Choose a cipher: ");
+        printList(ciphers);
         return ciphers.get(Integer.parseInt(scanner.nextLine()));
     }
 
     /**
-     * EFFECTS: Pretty-prints a given Arraylist of ciphers with indices
+     * EFFECTS: Pretty-prints a given Arraylist of elements with indices
      * @param list A list to be printed
      */
-    private void printCiphers(ArrayList<Cipher> list) {
+    private void printList(List list) {
         for (int i = 0; i < list.size(); i++) {
             System.out.println(i + " : " + list.get(i));
         }
     }
 
+    /**
+     * MODIFIES: this
+     * EFFECTS: Creates a new sequence
+     */
+    private void newSequence() {
+        sequences.add(new CipherSequence());
+        System.out.println("Added sequence: " + sequences.get(sequences.size() - 1));
+    }
+
+    /**
+     * EFFECTS: Prints out menus for modifying a sequence
+     */
+    private void modifySequence() {
+        CipherSequence sequence = chooseSequence();
+        System.out.println("Choose an action:");
+        System.out.println("l : List Ciphers in Sequence");
+        System.out.println("a : Add a cipher");
+        System.out.println("r : Remove a cipher");
+        System.out.println("e : Encode some text");
+        System.out.println("d : Decode some text");
+        processModifySequence(sequence);
+    }
+
+    /**
+     * REQUIRES: Legal console input
+     * MODIFIES: sequence
+     * EFFECTS: Processes user input and modifies a sequence.
+     * @param sequence The sequence to be modified.
+     */
+    private void processModifySequence(CipherSequence sequence) {
+        switch (scanner.nextLine()) {
+            case "l":
+                printList(sequence.getCipherList());
+                break;
+            case "a":
+                addToSequence(sequence);
+                break;
+            case "r":
+                removeFromSequence(sequence);
+                break;
+            case "e":
+                System.out.println(TRANSFORM_TEXT_PROMPT);
+                System.out.println(sequence.seriesEncode(scanner.nextLine()));
+                break;
+            case "d":
+                System.out.println(TRANSFORM_TEXT_PROMPT);
+                System.out.println(sequence.seriesDecode(scanner.nextLine()));
+                break;
+        }
+    }
+
+    /**
+     * REQUIRES: Legal console input
+     * MODIFIES: sequence
+     * EFFECTS: Adds a cipher to a sequence
+     * @param sequence The sequence to be added to.
+     */
+    private void addToSequence(CipherSequence sequence) {
+        System.out.println(sequence.getCipherList());
+        System.out.println("Enter the index at which to insert:");
+        int index = Integer.parseInt(scanner.nextLine());
+        System.out.println("Choose a Cipher to add:");
+        System.out.println(ALL_CIPHERS);
+        switch (Integer.parseInt(scanner.nextLine())) {
+            case 0:
+                sequence.addCipher(new AtbashCipher(), index);
+                System.out.println("Cipher added.");
+                break;
+            case 1:
+                sequence.addCipher(new Rot13Cipher(), index);
+                System.out.println("Cipher added.");
+                break;
+            case 2:
+                System.out.println("Enter a shift number greater than 0:");
+                sequence.addCipher(new CaesarCipher(Integer.parseInt(scanner.nextLine())), index);
+                System.out.println("Cipher added.");
+                break;
+            default:
+                System.out.println("Unknown Cipher.");
+        }
+    }
+
+    /**
+     * REQUIRES: Legal console input
+     * MODIFIES: sequence
+     * EFFECTS: Removes a cipher from the sequence
+     * @param sequence The sequence to be operated on.
+     */
+    private void removeFromSequence(CipherSequence sequence) {
+        System.out.println("Enter the index of the cipher to remove:");
+        printList(sequence.getCipherList());
+        sequence.removeCipher(Integer.parseInt(scanner.nextLine()));
+    }
+
+    /**
+     * REQUIRES: Legal console input
+     * EFFECTS: Gets the sequence chosen by user
+     * @return The sequence chosen by user
+     */
+    private CipherSequence chooseSequence() {
+        System.out.println("Choose a sequence:");
+        printList(sequences);
+        return sequences.get(Integer.parseInt(scanner.nextLine()));
+    }
 }
