18a9fb23af57b82a368b783c1dcb785fc4eed295
Jason Hsu <jasonhsu2013@gmail.com>
2020-02-11 18:58:50 -0800

    Add method stubs to CipherSequence

--- src/main/model/CipherSequence.java
+++ src/main/model/CipherSequence.java
@@ -5,9 +5,70 @@ import model.ciphers.Cipher;
 import java.util.LinkedList;
 
 /**
- * Represents a linear combination of Ciphers. Stores a sequence of Ciphers that can be applied to text.
+ * Represents a sequence of Ciphers. Stores a sequence of Ciphers that can be applied to text.
  */
 public class CipherSequence {
     private LinkedList<Cipher> internalList;
-    // todo
+
+    public CipherSequence() {
+        internalList = new LinkedList<>();
+    }
+
+    /**
+     * EFFECTS: Returns a copy of the internal list.
+     * @return A copy of the internal list
+     */
+    public LinkedList<Cipher> getCipherList() {
+        return new LinkedList<Cipher>(internalList);
+    }
+
+    /**
+     * MODIFIES: this
+     * EFFECTS: Adds a cipher to the end of the sequence
+     */
+    public void pushCipher(Cipher cipher) {
+        //todo
+    }
+
+    /**
+     * REQUIRES: Index within list size
+     * MODIFIES: this
+     * EFFECTS: Inserts a cipher at given index of the sequence
+     *
+     * @param cipher The cipher to insert
+     * @param index The index at which to insert
+     */
+    public void addCipher(Cipher cipher, int index) {
+        //todo
+    }
+
+    /**
+     * REQUIRES: Index within list size
+     * MODIFIES: this
+     * EFFECTS: Removes the cipher at the given index
+     * @param index The index of cipher of which to remove
+     */
+    public void removeCipher(int index) {
+        //todo
+    }
+
+    /**
+     * REQUIRES: text must be comprised of [A-Z] [a-z]
+     * EFFECTS: Encodes the text with each cipher in the sequence, in series.
+     * @param text The text to be encoded
+     * @return The encoded text
+     */
+    public String seriesEncode(String text) {
+        return null; //todo
+    }
+
+    /**
+     * REQUIRES: text has been encoded by this a CipherSequence with equal internal lists
+     * EFFECTS: Decodes the text with the inverse of seriesEncode
+     * @param text The text to be decoded
+     * @return The decoded text
+     */
+    public String seriesDecode(String text) {
+        return null; //todo
+    }
 }
