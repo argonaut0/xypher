337191c9badf590945da95f2eb1d91f94b5c3fdf
Jason Hsu <jasonhsu2013@gmail.com>
2020-02-11 19:03:30 -0800

    Add test stubs for CipherSequence

--- src/test/model/CipherSequenceTest.java
+++ src/test/model/CipherSequenceTest.java
@@ -4,10 +4,45 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CipherSequenceTest {
-    CipherSequence sequence = new CipherSequence();
+    CipherSequence sequence;
 
     @BeforeEach
     void runBefore() {
+        sequence = new CipherSequence();
+    }
+
+    @Test
+    void getCipherListTest() {
+        //todo
+    }
+
+    @Test
+    void getLengthTest() {
+        //todo
+    }
+
+    @Test
+    void pushCipherTest() {
+        //todo
+    }
+
+    @Test
+    void addCipherTest() {
+        //todo
+    }
+
+    @Test
+    void removeCipherTest() {
+        //todo
+    }
+
+    @Test
+    void seriesEncodeTest() {
+        //todo
+    }
+
+    @Test
+    void seriesDecodeTest() {
         //todo
     }
 }
