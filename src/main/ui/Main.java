a09a53710fb476ad7b556049ae0fc8a1132d59ce
Jason Hsu <jasonhsu2013@gmail.com>
2020-02-11 22:11:52 -0800

    Add documentation to Main

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,5 +1,9 @@
 package ui;
 
+/**
+ * Main class - Represents the entry point to the application
+ * @author Jason Hsu
+ */
 public class Main {
     public static void main(String[] args) {
         new CommandLineApp();
