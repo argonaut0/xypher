260df7a6a2a28199288e9950b75789fea7c1d688
Jason Hsu <jasonhsu2013@gmail.com>
2020-02-18 20:07:26 -0800

    Start refactor for cli

---
+++ src/main/ui/ClicheCLI.java
@@ -0,0 +1,35 @@
+package ui;
+
+import asg.cliche.Command;
+import model.CipherSequence;
+import model.ciphers.AtbashCipher;
+import model.ciphers.Cipher;
+import model.ciphers.Rot13Cipher;
+
+import java.util.ArrayList;
+
+public class ClicheCLI {
+    private enum CipherList {
+        ATBASH,
+        ROT13,
+        CAESAR
+    }
+    private ArrayList<CipherSequence> sequences;
+    private ArrayList<Cipher> ciphers;
+
+    /**
+     * MODIFIES: this
+     * EFFECTS: Sets up fields
+     */
+    public ClicheCLI() {
+        sequences = new ArrayList<>();
+        ciphers = new ArrayList<>();
+        ciphers.add(new AtbashCipher());
+        ciphers.add(new Rot13Cipher());
+    }
+
+    @Command
+    public void addCipher(CipherList cipher) {
+
+    }
+}
