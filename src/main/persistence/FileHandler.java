2633fd7d3e570163204877dc01b3ea17a83e4e1c
Jason Hsu <jasonhsu2013@gmail.com>
2020-02-25 12:07:43 -0800

    Add Filehandler stub

---
+++ src/main/persistence/FileHandler.java
@@ -0,0 +1,14 @@
+package persistence;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+
+public class FileHandler {
+    private Gson gson;
+    private BufferedReader bufferedReader;
+
+    public FileHandler() {
+
+    }
+}
