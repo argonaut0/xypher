f0a9d16d881ff31c1a5647376cfd4b13d702dd93
Jason Hsu <jasonhsu2013@gmail.com>
2020-02-25 22:54:40 -0800

    Implement Cipher and Listing ui

--- src/main/ui/ClicheCLI.java
+++ src/main/ui/ClicheCLI.java
@@ -2,35 +2,128 @@ package ui;
 
 import asg.cliche.Command;
 import model.CipherSequence;
+import model.XypherApp;
 import model.ciphers.AtbashCipher;
-import model.ciphers.Cipher;
+import model.ciphers.CaesarCipher;
 import model.ciphers.Rot13Cipher;
 
-import java.util.ArrayList;
+import java.io.IOException;
 
 public class ClicheCLI {
-    private enum CipherList {
-        ATBASH,
-        ROT13,
-        CAESAR
-    }
 
-    private ArrayList<CipherSequence> sequences;
-    private ArrayList<Cipher> ciphers;
+    private XypherApp app;
+    //todo sequences
 
     /**
      * MODIFIES: this
      * EFFECTS: Sets up fields
      */
     public ClicheCLI() {
-        sequences = new ArrayList<>();
-        ciphers = new ArrayList<>();
-        ciphers.add(new AtbashCipher());
-        ciphers.add(new Rot13Cipher());
+        app = new XypherApp();
+    }
+
+    /**
+     *
+     * @param args At least 1 argument
+     */
+    @Command
+    public void addCipher(String... args) {
+        try {
+            switch (args[0]) {
+                case "atbash":
+                    app.addEncoder(new AtbashCipher());
+                    break;
+                case "rot13":
+                    app.addEncoder(new Rot13Cipher());
+                    break;
+                case "caesar":
+                    app.addEncoder(new CaesarCipher(Integer.parseInt(args[1])));
+                    break;
+                default:
+                    System.out.println("Unrecognized Cipher Type");
+                    break;
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Incorrect arguments");
+        }
     }
 
     @Command
-    public void addCipher(CipherList cipher) {
+    public void addSequence(String name) {
+        try {
+            app.addEncoder(new CipherSequence(name));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
+    @Command
+    public String listEncoders() {
+        return app.toString();
+    }
+
+    @Command
+    public void encode(String encoder, String text) {
+        try {
+            System.out.println(app.encode(encoder, text));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Command
+    public void decode(String encoder, String text) {
+        try {
+            System.out.println(app.decode(encoder, text));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Command
+    public void saveCipher(String name) {
+        try {
+            app.saveCipher(name);
+        } catch (IllegalArgumentException | IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Command
+    public void loadCipher(String name) {
+        try {
+            app.loadCipher(name);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Command
+    public void saveSequence(String name) {
+        try {
+            app.saveSequence(name);
+        } catch (IllegalArgumentException | IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Command
+    public void loadSequence(String name) {
+        try {
+            app.loadSequence(name);
+        } catch (IllegalArgumentException | IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Command
+    public void deleteEncoder(String name) {
+        try {
+            app.deleteEncoder(name);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
