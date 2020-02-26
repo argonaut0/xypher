e72e4ff71ae9a361263e030f01c693c9a7ea8004
Jason Hsu <jasonhsu2013@gmail.com>
2020-02-25 18:46:17 -0800

    Add FileHandler Test stubs

---
+++ src/test/persistence/FileHandlerTest.java
@@ -0,0 +1,41 @@
+package persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FileHandlerTest {
+    FileHandler fileHandler;
+
+    @BeforeEach
+    void runBefore() {
+        fileHandler = FileHandler.getInstance();
+    }
+
+    @Test
+    void singletonTest() {
+        assertEquals(fileHandler, FileHandler.getInstance());
+        assertEquals(FileHandler.getInstance(), FileHandler.getInstance());
+    }
+
+    @Test
+    void loadCipherTest() {
+        //todo
+    }
+
+    @Test
+    void loadSequenceTest() {
+        //todo
+    }
+
+    @Test
+    void saveCipherTest() {
+        //todo
+    }
+
+    @Test
+    void saveSequenceTest() {
+        //todo
+    }
+}
